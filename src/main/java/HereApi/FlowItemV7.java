d89fc38cb83b955f1988a35074c01f4ad6ec44cb
Yannik Dirlich <97292530+Y4nn1k8@users.noreply.github.com>
2022-06-30 10:24:15 +0200

    added javadoc

--- src/main/java/HereApi/FlowItemV7.java
+++ src/main/java/HereApi/FlowItemV7.java
@@ -3,6 +3,9 @@ package HereApi;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * container for information of a single Item from HERE's traffic flow API version 7
+ */
 public class FlowItemV7 {
 
     @Nullable
@@ -27,7 +30,34 @@ public class FlowItemV7 {
     private Integer posoff;
     @Nullable
     private Integer negoff;
-
+    
+    /**
+     * @param name street-name
+     * @param olr Open Location Reference
+     * @param speed expected speed along the roadway; doesn't exceed the legal speed limit.
+     * @param speedUncapped expected speed along the roadway; may exceed the legal speed limit.
+     * @param freeFlow reference speed along the roadway when no traffic is present.
+     * @param jamFactor value between 0 and 10; the higher, the more traffic. 10 stands for road closure.
+     * @param confidence indicates the proportion of real time data included in the speed calculation.
+     * It is a normalized value between 0.0 and 1.0 with the following meaning:
+     * 0.7 < confidence <= 1.0 indicates real time speeds
+     * 0.5 < confidence <= 0.7 indicates historical speeds
+     * 0.0 < confidence <= 0.5 indicates speed limit
+     * @param traversability The traversability describes whether the roadway is drivable.
+     * can be:
+     * open - the roadway can be driven
+     * closed - the roadway cannot be driven (jamFactor is 10.0)
+     * reversibleNotRoutable - the roadway is reversible and currently not routable
+     * @param junctionTraversability Used for road closures to indicate if the junctions along the closure can be crossed.
+     * can be:
+     * ALL_OPEN
+     * ALL_CLOSED
+     * INTERMEDIATE_CLOSED_EDGE_OPEN
+     * START_OPEN_OTHERS_CLOSED
+     * END_OPEN_OTHERS_CLOSED
+     * @param posoff needed for visualization in QGIS
+     * @param negoff needed for visualization in QGIS
+     */
     public FlowItemV7(@Nullable String name, @NotNull String olr, @Nullable Double speed,
                       @Nullable Double speedUncapped, @Nullable Double freeFlow,
                       @Nullable Double jamFactor, @Nullable Double confidence,
@@ -97,7 +127,7 @@ public class FlowItemV7 {
     public Double getSpeed() { return speed; }
 
     /**
-     * expected speed along the roadway; may exceed the legal speed limit
+     * expected speed along the roadway; may exceed the legal speed limit.
      * presumably given in meter/second
      */
     @Nullable
