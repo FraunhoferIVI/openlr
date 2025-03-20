431b8b8aa3c0c0dc05492fcb8ed20faa46af1355
Deepak Barajati <deepakbarajati2004@gmail.com>
2025-03-20 11:04:00 +0530

    Update TrafficItem.java

--- src/main/java/HereApi/TrafficItem.java
+++ src/main/java/HereApi/TrafficItem.java
@@ -1,82 +1,28 @@
 package HereApi;
-
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 /**
  * The traffic item object read from the Traffic API requested xml.
  *
  * @author Emily Kast
  */
-
+@Getter        
+@AllArgsConstructor 
+@NoArgsConstructor   
 public class TrafficItem {
 
     private String id;
-    private String type;
     private String status;
+    private String type;
     private String start;
     private String end;
     private String criticality;
     private String openLR;
+    private String closure;
     private String shortDesc;
     private String longDesc;
-    private String closure;
-
-    public TrafficItem(String id, String status, String type, String start, String end, String criticality, String openLR, String closure, String shortDesc, String longDesc) {
-
-        this.id = id;
-        this.status = status;
-        this.type = type;
-        this.start = start;
-        this.end = end;
-        this.criticality = criticality;
-        this.openLR = openLR;
-        this.closure = closure;
-        this.shortDesc = shortDesc;
-        this.longDesc = longDesc;
-    }
-
-    public TrafficItem() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public String getStart() {
-        return start;
-    }
-
-    public String getCriticality() {
-        return criticality;
-    }
-
-    public String getOpenLR() {
-        return openLR;
-    }
-
-    public String getEnd() {
-        return end;
-    }
-
-    public String getShortDesc() {
-        return shortDesc;
-    }
-
-    public String getLongDesc() {
-        return longDesc;
-    }
-
-    public String getClosure() {
-        return closure;
-    }
-
-
+   
     @Override
     public String toString() {
         return "HereApi.TrafficItem [id = " + id + ", status = " + status + ", type = " + type + ", start = " + start +
